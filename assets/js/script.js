9edd6e8695262b472849a1ff2fc6330bcc0a50f2
Tonyio83 <anthonyfrancelle@gmail.com>
2019-11-06 09:14:26 +0100

    exercice 5 correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
-$('#lastName, #firstName').focus(function(){//crée une fonction avec le focus sur les input
-  $(this).css('border','1px solid green');//met la bordure en vert sur l'input
-})
-$('#lastName, #firstName').focusout(function(){//crée une fonction avec le focus hors des input
-  $(this).css('border','1px solid red');//met la bordure en rouge sur l'input
+$(function(){
+  $('#lastName, #firstName').focus(function(){//appel de la fonction avec le focus sur les input
+    $(this).css('border','1px solid green');//met la bordure en vert sur l'input
+  })
+  $('#lastName, #firstName').focusout(function(){//appel de la fonction avec le focus hors des input
+    $(this).css('border','1px solid red');//met la bordure en rouge sur l'input
+  })
 })
